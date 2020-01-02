b9dd4461619bda0146ac27cc3059f5e94096a363
naari3 <naari.named@gmail.com>
2020-01-03 00:40:22 +0900

    Implemented status

--- src/Status.tsx
+++ src/Status.tsx
@@ -1,7 +1,39 @@
-import React, { useState } from "reactn";
+import React, { useState, useEffect, useGlobal } from "reactn";
+// "Pending" | "InService" | "Terminating" | "Terminated"
+const statusMessages = {
+  Pending: "たててます",
+  InService: "やってます",
+  Terminating: "おとしてます",
+  Terminated: "やってません"
+};
 
 const Status: React.FC = () => {
-  const [running] = useState(false);
+  const [running, setRunning] = useState(statusMessages.Terminated);
+  const [, setMessage] = useGlobal("message");
+
+  useEffect(() => {
+    const intervalId = setInterval(async () => {
+      const req = await fetch(
+        `${process.env.REACT_APP_API_HOST}/instance_status`,
+        {
+          credentials: "include",
+          mode: "cors"
+        }
+      );
+      if (req.status !== 200) {
+        setMessage("なんかおかしい、連絡してください");
+      }
+      const status = (await req.json()).status as
+        | "Pending"
+        | "InService"
+        | "Terminating"
+        | "Terminated";
+      setRunning(statusMessages[status]);
+    }, 10000);
+    return (): void => {
+      clearInterval(intervalId);
+    };
+  });
   return (
     <div className="Status">
       {running ? <p>やってます</p> : <p>やってません</p>}
