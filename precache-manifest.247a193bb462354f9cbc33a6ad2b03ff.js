be902919da8ddf4b1671691e3129f54e95e5a89e
naari3 <naari.named@gmail.com>
2020-01-06 04:28:35 +0900

    Updates

---
+++ precache-manifest.247a193bb462354f9cbc33a6ad2b03ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1678fcbcb8292a3b1661f518724858a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab470d4cab0e8d99fe54",
+    "url": "/static/css/main.1bdddab5.chunk.css"
+  },
+  {
+    "revision": "46facc4f2395cd87fc9e",
+    "url": "/static/js/2.4984f7f6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.4984f7f6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ab470d4cab0e8d99fe54",
+    "url": "/static/js/main.b962290a.chunk.js"
+  },
+  {
+    "revision": "95edba82dc238220ef3e",
+    "url": "/static/js/runtime-main.2f69602f.js"
+  }
+]);
